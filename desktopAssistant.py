8d4ec2b66b09dfd3d25a76063f7e09aaea58be9e
Chaboddunamis <henry.uwakxy@gmail.com>
2020-06-18 00:17:30 +0100

    Add files via upload

---
+++ desktopAssistant.py
@@ -0,0 +1,261 @@
+from gtts import gTTS
+import speech_recognition as sr
+import os
+import re
+import webbrowser
+import smtplib
+import pyttsx3
+import requests
+import subprocess
+from pyowm import OWM
+import youtube_dl
+import urllib3
+import json
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+import wikipedia
+import random
+from time import strftime
+from weather import Weather
+from playsound import playsound
+
+engine = pyttsx3.init()
+engine.say("Hello Henry. Jarvis is initializing")
+engine.runAndWait()
+
+def talkToMe(audio):
+    "speaks audio passed as argument"
+
+    print(audio)
+    for line in audio.splitlines():
+        os.system("say" + audio)
+
+    #  use the system's inbuilt say command instead of mpg123
+    text_to_speechx3 = gTTS(text=audio, lang='en')
+    text_to_speechx3.save('audio.mp3')
+    playsound('audio.mp3')
+
+
+def myCommand():
+    "listens for commands"
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('I am ready for your next command Master Henry...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=2)
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print('You said: ' + command + '\n')
+
+    #loop back to continue to listen for commands if unrecognizable speech is received
+    except sr.UnknownValueError:
+        print('Your last command couldn\'t be heard')
+        command = myCommand();
+
+    return command
+
+
+def assistant(command):
+    "if statements for executing commands"
+
+    if 'open reddit' in command: 
+        reg_ex = re.search('open reddit (.*)', command)
+        url = 'https://www.reddit.com/'
+        webbrowser.open(url)
+        if reg_ex:
+            subreddit = reg_ex.group(1)
+            url = url + 'r/' + subreddit
+        webbrowser.open(url)
+        print('Done!')
+
+    elif 'open' in command:
+        reg_ex = re.search('open (.+)', command)
+        if reg_ex:
+            domain = reg_ex.group(1)
+            print(domain)
+            url = 'https://www.' + domain
+            webbrowser.open(url)
+            print('Done!')
+        else:
+            pass
+
+    elif 'what\'s up' in command:
+        talkToMe('Just doing my thing')
+    elif 'joke.' in command:
+        res = requests.get(
+                'https://icanhazdadjoke.com/',
+                headers={"Accept":"application/json"}
+                )
+        if res.status_code == requests.codes.ok:
+            talkToMe(str(res.json()['joke']))
+        else:
+            talkToMe('oops!I ran out of jokes')
+
+    elif 'current weather in' in command:
+        reg_ex = re.search('current weather in (.*)', command)
+        if reg_ex:
+            city = reg_ex.group(1)
+            weather = Weather()
+            location = weather.lookup_by_location(city)
+            condition = location.condition()
+            talkToMe('The Current weather in %s is %s The tempeture is %.1f degree' % (city, condition.text(), (int(condition.temp())-32)/1.8))
+
+    elif 'weather forecast in' in command:
+        reg_ex = re.search('weather forecast in (.*)', command)
+        if reg_ex:
+            city = reg_ex.group(1)
+            weather = Weather()
+            location = weather.lookup_by_location(city)
+            forecasts = location.forecast()
+            for i in range(0,3):
+                talkToMe('On %s will it %s. The maximum temperture will be %.1f degree.'
+                         'The lowest temperature will be %.1f degrees.' % (forecasts[i].date(), forecasts[i].text(), (int(forecasts[i].high())-32)/1.8, (int(forecasts[i].low())-32)/1.8))
+
+
+    elif 'email' in command:
+        talkToMe('Who is the recipient?')
+        recipient = myCommand()
+
+        if 'Henry' in recipient:
+            talkToMe('What should I say?')
+            content = myCommand()
+
+            #init gmail SMTP
+            mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+            #identify to server
+            mail.ehlo()
+
+            #encrypt session
+            mail.starttls()
+
+            #login
+            mail.login('Henry.uwakxy@gmail.com', 'Chaboddunamis')
+
+            #send message
+            mail.sendmail('Henry Uwakwe', 'uwaks4arsenal@yahoomail.com', content)
+
+            #end mail connection
+            mail.close()
+
+            talkToMe('Email sent.')
+
+        else:
+            talkToMe('I don\'t know what you mean!')
+
+
+
+
+#Launch any application
+
+    elif 'launch' in command:
+        reg_ex = re.search('launch (.*)', command)
+        if reg_ex:
+            appname = reg_ex.group(1)
+            appname1 = appname+".app"
+            subprocess.Popen(["open", "-n", "/Applications/" + appname1], stdout=subprocess.PIPE)
+            talkToMe('Done!')
+        
+
+    elif 'time' in command:
+          import datetime
+          now = datetime.datetime.now()
+          talkToMe('Current time is %d hours %d minutes' % (now.hour, now.minute))
+        
+
+        #Greet Sofia
+    elif 'hello' in command:
+        day_time = int(strftime('%H'))
+        if day_time < 12:
+            talkToMe('Hello Sir. Good morning')
+        elif 12 <= day_time < 18:
+            talkToMe('Hello Sir. Good afternoon')
+        else:
+            talkToMe('Hello Sir. Good evening')
+
+#to terminate the program
+    elif 'shutdown' in command:
+     talkToMe('Bye bye Sir. Have a nice day')
+     sys.exit()
+    
+
+
+    elif 'change wallpaper' in command:
+        folder = '/Users/nageshsinghchauhan/Documents/wallpaper/'
+        for the_file in os.listdir(folder):
+            file_path = os.path.join(folder, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        api_key = 'fd66364c0ad9e0f8aabe54ec3cfbed0a947f3f4014ce3b841bf2ff6e20948795'
+        url = 'https://api.unsplash.com/photos/random?client_id=' + api_key #pic from unspalsh.com
+        f = urllib3.urlopen(url)
+        json_string = f.read()
+        f.close()
+        parsed_json = json.loads(json_string)
+        photo = parsed_json['urls']['full']
+        urllib3.urlretrieve(photo, "/Users/nageshsinghchauhan/Documents/wallpaper/a") # Location where we download the image to.
+        subprocess.call(["killall Dock"], shell=True)
+        talkToMe('wallpaper changed successfully')
+
+    
+
+
+
+
+    elif 'news for today' in command:
+        try:
+            news_url="https://news.google.com/news/rss"
+            Client=urlopen(news_url)
+            xml_page=Client.read()
+            Client.close()
+            soup_page=soup(xml_page,"xml")
+            news_list=soup_page.findAll("item")
+            for news in news_list[:15]:
+                talkToMe(news.title.text.encode('utf-8'))
+        except Exception as e:
+                print(e)
+
+
+
+
+    elif 'tell me about' in command:
+        reg_ex = re.search('tell me about (.*)', command)
+        try:
+            if reg_ex:
+                topic = reg_ex.group(1)
+                ny = wikipedia.page(topic)
+                talkToMe(ny.content[:500].encode('utf-8'))
+        except Exception as e:
+                talkToMe(e)
+
+
+    
+
+    elif 'help me' in command:
+        talkToMe("""
+        You can use these commands and I'll help you out:
+        1. Open reddit subreddit : Opens the subreddit in default browser.
+        2. Open xyz.com : replace xyz with any website name
+        3. Send email/email : Follow up questions such as recipient name, content will be asked in order.
+        4. Current weather in {cityname} : Tells you the current condition and temperture
+        5. Hello
+        6. play me a video : Plays song in your VLC media player
+        7. change wallpaper : Change desktop wallpaper
+        8. news for today : reads top news of today
+        9. time : Current system time
+        10. top stories from google news (RSS feeds)
+        11. tell me about xyz : tells you about xyz
+        """)
+
+talkToMe('Ready for your command')
+
+#loop to continue executing multiple commands
+while True:
+    assistant(myCommand())
